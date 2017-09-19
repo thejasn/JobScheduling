744bc0d4346f2d7496315c80bc1c157ffab2bd09
Thejasn <thejasn.416@gmail.com>
2017-09-19 23:26:50 +0530

    Turnaround Time done

--- sjf_pre.c
+++ sjf_pre.c
@@ -16,12 +16,11 @@ void getData(PROCESS **list,int size)
         list[i] = (PROCESS*)calloc(1,sizeof(PROCESS));
         scanf("%d%d",&list[i]->burst_t,&list[i]->arr_t);
         list[i]->p_id = i;
-        list[i]->touched=0;
-       
 
     }
+    
 }
-void sort_busrt(PROCESS **list, int size,int start,int end)
+/*void sort_busrt(PROCESS **list, int size,int start,int end)
 {
     PROCESS *temp;
     int i,j;
@@ -47,7 +46,7 @@ void sort_arr(PROCESS **list, int size,int start,int end)
                 list[j] = list[j+1];
                 list[j+1] = temp;
             }
-}
+}*/
 void show_data(PROCESS  **list, int size)
 {
     int i;
@@ -57,80 +56,51 @@ void show_data(PROCESS  **list, int size)
         
     }
 }
+
 int least_burst(PROCESS **list,int till,int size)
 {
-    //printf("helo");
-    int j;
-    //for(j=0;j<size;j++)
-      //  printf("\n%d",list[j]->p_id);
-    int i=1,pos=0,max=9999;
-    while(list[i]->arr_t <= till && i < size)
-    {   // printf("\n%d",till);
-        if(list[i]->burst_t > 0)
-        {   
-            if(list[i]->burst_t <= list[pos]->burst_t )//&& list[i]->p_id != list[pos]->p_id)
-            {    
-               pos = i;
-               
-               //break;
-               //printf("\np%d",pos);
-               fflush(stdout);
-               break;
-            }
-
-
+   
+    list[size-1] = (PROCESS*)calloc(1,sizeof(PROCESS));
+    list[size-1]->burst_t=99999;
+    int least = size-1;
+    
+    int i;
+    for(i=0;i<size;i++)
+    {
+        if(list[i]->arr_t <= till && list[i]->burst_t < list[least]->burst_t && list[i]->burst_t > 0)
+        {
+            least = i;
         }
-        i++;
-      //  printf("\ni%d",i);
-        //fflush(stdout);
     }
-    printf("\np%d",pos);
-    fflush(stdout);
-    if(i == size)
-        return 0;
-    return pos;        
+    return least;
 }
 void SJF(PROCESS ** list,int size)
 {
-   // printf("helo");
-   int max; 
-    int count_timer=0;
-    do{
-        //printf("\nhel");
-        int until = least_burst(list,count_timer,size);
-       printf("\nu%d",until);
-       fflush(stdout); 
-       //printf("\nhello");
-        if(!list[until]->touched)
-        {   list[until]->touched=1;       
-            list[until]->start=count_timer;
-            //max = list[until]->burst_t;
-        }
+    int count_timer=0,end,num=0;
+    for(count_timer=0; num != size ; count_timer++){
+        int until = least_burst(list,count_timer,size+1);
+       
         list[until]->burst_t--;
-        //list[until]->turn_t=list[until]->turn_t+1;
-        //printf("\nhello");
+       
         if ( list[until]->burst_t == 0)
-        {           
-            list[until]->turn_t = (count_timer - list[until]->arr_t + 1);
-            printf("\n%d",list[until]->turn_t);
-             
-
+        {  
+            num++;
+            //count_timer++; 
+            end = count_timer+1;        
+            list[until]->turn_t = (end - list[until]->arr_t );
+            printf("\n%d,%d",list[until]->turn_t,end);
         }
-        //printf("\nhello");
-        count_timer++;
-       // printf("\nhello");
-        printf("\nc%d",count_timer);
-        fflush(stdout);
-    }while(count_timer < 19);
+     
+    }
 }
 int main()
 {
     int size;
     scanf("%d", &size);
-    PROCESS ** list = (PROCESS**)calloc(size,sizeof(PROCESS));
+    PROCESS ** list = (PROCESS**)calloc(size+1,sizeof(PROCESS));
     //PROCESS list[100];
     getData(list,size);
-    sort_arr(list,size,0,size-1);
+    //sort_arr(list,size,0,size-1);
     SJF(list,size);
     show_data(list,size);
     return 0;
